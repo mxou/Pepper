a674b7db6cc3090b53b2a48f59497cd2f441b2de
mxou <maximilien.dlrm@gmail.com>
2026-05-04 07:18:48 +0200

    lecture dossier

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Démarrage");
+
+const username = "mxou";
+const fs = require("fs");
+const path = require("path");
+const downloadFile = `C:/Users/${username}/Downloads`;
+
+const fichier = fs.readdirSync(downloadFile);
+
+console.log(fichier);
